0210b1f679c93e871a5964b47c9abdadef4bb443
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-04-17 22:38:10 +0200

    Start of hero styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,40 @@
 
 </script>
 
-
+<section class="hero">
+    <div class="hero-text">
+        <div class="hero-text-upper">
+            <h1>Tristan <span>de Muijnck</span></h1>
+            <p class="rotated">FDND</p>
+        </div>
+        <p>App developer &amp; student frontend design and development</p>
+    </div>
+    
+    <a href="/projects">Projects</a>
+</section>
 
 <style>
+    .hero{
+        width: 80%;
+        margin: 0 auto;
+    }
 
+    .hero-text{
+        display: flex;
+        flex-direction: column;
+    }
+
+    .hero-text-upper{
+        display: flex;
+        flex-direction: row;
+    }
+
+    .rotated{
+        transform: rotate(-90deg);
+        margin-left: 2rem;
+    }
+
+    span{
+        display: block;
+    }
 </style>
