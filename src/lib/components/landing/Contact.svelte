3828498b53053d971e8d7574ecea621ba6e05501
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-05-18 19:52:44 +0200

    V1 mobile && ADDED contact mobile css

--- src/lib/components/landing/Contact.svelte
+++ src/lib/components/landing/Contact.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <section class="contact" id="contact">
-    
+
     <!-- Contact heading text -->
-    <h2>Contact</h2>
+    <h2>CONTACT</h2>
 
     <!-- Scroll buttons menu -->
     <ScrollButtons />
@@ -16,9 +16,11 @@
     <!-- Contact info -->
     <div class="contact-info">
         <div class="contact-left">
-            <img src="/img/icons/github.svg" alt="" class="icon" width="65" height="65">
-            <img src="/img/icons/linkedin.svg" alt="" class="icon" width="65" height="65">
-            <img src="/img/icons/instagram.svg" alt="" class="icon" width="65" height="65">
+            <div class="icons">
+                <a href="https://github.com/Tristandemuijnck"><img src="/img/icons/github.svg" alt="" class="icon" width="65" height="65"></a>
+                <a href=""><img src="/img/icons/linkedin.svg" alt="" class="icon" width="65" height="65"></a>
+                <a href=""><img src="/img/icons/instagram.svg" alt="" class="icon" width="65" height="65"></a>
+            </div>
             <p class="contact-text">Tristandemuijnck</p>
         </div>
         <div class="contact-right">
@@ -30,5 +32,80 @@
 </section>
 
 <style>
+    .contact{
+        width: 100%;
+        min-height: 50vh;
+        display: flex;
+        flex-direction: column;
+        position: relative;
+    }
 
+    h2{
+        font-size: 4em;
+        font-weight: normal;
+        position: relative;
+        text-transform: uppercase;
+        padding-left: 2rem;
+        margin-bottom: 4rem;
+    }
+
+    h2::after{
+        content: "CONTACT";
+        white-space: pre;
+        color: transparent;
+        position: absolute;
+        left: 2px;
+        top: -2px;
+        -webkit-text-stroke: 1px #fff;
+        padding-left: 2rem;
+    }
+
+    .bg-text{
+        display: none;
+    }
+
+    .contact-info{
+        width: 90%;
+        margin: 0 auto 4rem auto;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .contact-left{
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        gap: 2rem;
+    }
+
+    .icons{
+        display: flex;
+        gap: 1rem;
+    }
+
+    .icon{
+        width: 2rem;
+        height: auto;
+    }
+
+    .contact-text{
+        font-size: 1.3em;
+        font-weight: bold;
+        text-transform: uppercase;
+    }
+
+    .contact-left::after{
+        content: "";
+        width: 66%;
+        height: 1px;
+        background-color: var(--third-color);
+    }
+
+    .contact-right{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 2rem;
+        margin-top: 2rem;
+    }
 </style>
